1798c64c053df7fc712d38cc5aba9c3ab73e8c8e
rushikesh-3 <shivanangaaji@gmail.com>
2026-05-26 13:59:08 +0530

    Updated Day2 files

---
+++ DAY-1/Product Management/app.js
@@ -0,0 +1,6 @@
+const productList = document.getElementById('product-list');
+const addProductForm = document.getElementById('add-product-form');
+const productNameInput = document.getElementById('product-name');
+const productPriceInput = document.getElementById('product-price');
+
+
